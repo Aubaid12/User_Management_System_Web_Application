cbd7f70a501d448f688819e49be578392c636b78
Aubaid <72610395+Aubaid12@users.noreply.github.com>
2023-02-26 15:45:08 +0530

    commit2

--- User_Management_System_Application/settings.py
+++ User_Management_System_Application/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polls'
 
+
 ]
 
 MIDDLEWARE = [
@@ -122,10 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
